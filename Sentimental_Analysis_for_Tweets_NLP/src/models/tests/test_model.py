04bd22f4d0b093a3e98a6fce2ea1ceb08eba5995
liangc40 <frank_liang@Chens-MacBook-Pro.local>
2022-01-18 06:43:55 +0100

    pytests

---
+++ Sentimental_Analysis_for_Tweets_NLP/src/models/tests/test_model.py
@@ -0,0 +1,43 @@
+import sys
+sys.path.append(".")
+
+# from TweetsDataset import *
+from TweetsDataset import TweetsDataset
+import transformers
+from transformers import BertModel
+from functions import create_data_loader
+from model import DepressionClassifier
+import numpy as np
+from torch import nn
+import pandas as pd
+import torch
+from transformers import BertTokenizer
+from sklearn.model_selection import train_test_split
+
+MAX_LEN = 160
+BATCH_SIZE = 16
+PRE_TRAINED_MODEL_NAME = 'bert-base-cased'
+class_names = ['Not Depressed', 'Depressed']
+
+df = pd.read_csv(f'sentiment_tweets3.csv', encoding = 'latin-1')
+tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
+
+df_train, df_test = train_test_split(df, test_size = 0.2, random_state = 42)
+data_loader = create_data_loader(df_train, tokenizer, max_len=MAX_LEN, batch_size=BATCH_SIZE)
+first_data =  next(iter(data_loader))
+input_ids = first_data['input_ids']
+attention_mask = first_data['attention_mask']
+
+def test_DepressionClassifier_init():
+    model = DepressionClassifier(len(class_names), PRE_TRAINED_MODEL_NAME)
+    assert type(model.bert) == transformers.models.bert.modeling_bert.BertModel
+    assert type(model.drop) == nn.Dropout
+    assert type(model.out) == nn.Linear
+
+def test_DepressionClassifier_forward():
+    model = DepressionClassifier(len(class_names), PRE_TRAINED_MODEL_NAME)
+    out = model(input_ids, attention_mask)
+    assert first_data['input_ids'].shape == torch.Size([BATCH_SIZE, MAX_LEN])
+    assert first_data['attention_mask'].shape == torch.Size([BATCH_SIZE, MAX_LEN])
+    assert out.ndim == 2
+    assert out.shape == torch.Size([BATCH_SIZE, 2])
---
+++ Sentimental_Analysis_for_Tweets_NLP/src/models/tests/test_training.py
@@ -0,0 +1,62 @@
+import sys
+sys.path.append(".")
+
+# from TweetsDataset import *
+from TweetsDataset import TweetsDataset
+import transformers
+from transformers import AdamW, get_linear_schedule_with_warmup
+from functions import create_data_loader, train_epoch, eval_model, loss_accuracy_plots
+from model import DepressionClassifier
+import numpy as np
+from torch import nn
+import pandas as pd
+from collections import defaultdict
+import torch
+from transformers import BertTokenizer
+from sklearn.model_selection import train_test_split
+
+MAX_LEN = 160
+BATCH_SIZE = 16
+EPOCHS = 1
+PRE_TRAINED_MODEL_NAME = 'bert-base-cased'
+class_names = ['Not Depressed', 'Depressed']
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+df = pd.read_csv(f'sentiment_tweets3.csv', encoding = 'latin-1')
+tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
+loss_fn = nn.CrossEntropyLoss()
+
+df_train = df.head(BATCH_SIZE)
+df_val = df.tail(BATCH_SIZE)
+train_data_loader = create_data_loader(df_train, tokenizer, max_len=MAX_LEN, batch_size=BATCH_SIZE)
+val_data_loader = create_data_loader(df_val, tokenizer, max_len=MAX_LEN, batch_size=BATCH_SIZE)
+model = DepressionClassifier(len(class_names), PRE_TRAINED_MODEL_NAME)
+
+optimizer = AdamW(model.parameters(), lr = 2e-5, correct_bias = False)
+total_steps = len(train_data_loader) * EPOCHS
+scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps = 0,
+                                                num_training_steps = total_steps)
+
+history = defaultdict(list)
+output_path = "../"
+
+
+def test_train_epoch():
+    train_acc, train_loss = train_epoch(model,
+    train_data_loader, loss_fn, optimizer, device, scheduler, len(df_train))
+    assert type(train_acc) == torch.Tensor
+    assert train_acc >= 0
+    assert train_acc <= 1
+    assert train_loss >= 0
+
+def test_eval_model():
+    val_acc, val_loss = eval_model(model,
+    val_data_loader, loss_fn, device, len(df_val))
+
+    assert type(val_acc) == torch.Tensor
+    assert val_acc >= 0
+    assert val_acc <= 1
+    assert val_loss >= 0
+
+def test_loss_accuracy_plots():
+    loss_accuracy_plots(history, output_path)
