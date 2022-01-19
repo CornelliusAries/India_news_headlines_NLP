2f6c23c98c833401dcab808da3858e6a801a6a3d
liangc40 <frank_liang@Chens-MacBook-Pro.local>
2022-01-19 11:28:22 +0100

    with lightning model

---
+++ Sentimental_Analysis_for_Tweets_NLP/src/models/data.py
@@ -0,0 +1,58 @@
+import pytorch_lightning as pl
+from transformers import BertTokenizer
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from torch.utils.data import DataLoader
+from TweetsDataset import TweetsDataset
+
+
+PRE_TRAINED_MODEL_NAME = 'bert-base-cased'
+MAX_LEN = 160
+BATCH_SIZE = 16
+EPOCHS = 10
+    
+df = pd.read_csv('sentiment_tweets3.csv', encoding = 'latin-1')
+tokenizer = BertTokenizer.from_pretrained(PRE_TRAINED_MODEL_NAME)
+
+df_train, df_test = train_test_split(df, test_size = 0.2, random_state = 42)
+df_train, df_test = train_test_split(df, test_size = 0.2, random_state = 42)
+df_val, df_test = train_test_split(df_test, test_size = 0.5, random_state = 42)
+
+
+class DataModule(pl.LightningDataModule):
+  def setup(self, stage):
+    def __init__(self, data_dir, batch_size):
+        super().__init__()
+        self.data_dir = data_dir
+        self.batch_size = batch_size
+
+        # df = pd.read_csv(self.data_dir, encoding = 'latin-1')
+        # self.tokenizer = BertTokenizer.from_pretrained(PRE_TRAINED_MODEL_NAME)
+
+        # df_train, df_test = train_test_split(df, test_size = 0.2, random_state = 42)
+        # df_train, df_test = train_test_split(df, test_size = 0.2, random_state = 42)
+        # df_val, df_test = train_test_split(df_test, test_size = 0.5, random_state = 42)
+
+        # self.df_train = df_train
+        # self.df_val = df_val
+        # self.df_test = df_test
+
+      
+    self.train_dataset = TweetsDataset(
+        message = df_train['message to examine'].to_numpy(),
+        depression = df_train['label (depression result)'].to_numpy(),
+        tokenizer=tokenizer,
+        max_len=MAX_LEN)
+    
+    self.val_dataset = TweetsDataset(
+        message = df_val['message to examine'].to_numpy(),
+        depression = df_val['label (depression result)'].to_numpy(),
+        tokenizer=tokenizer,
+        max_len=MAX_LEN)
+    
+
+  def train_dataloader(self):
+    return DataLoader(self.train_dataset, batch_size=BATCH_SIZE, num_workers = 0)
+
+  def val_dataloader(self):
+    return DataLoader(self.train_dataset, batch_size=BATCH_SIZE, num_workers = 0)
--- Sentimental_Analysis_for_Tweets_NLP/src/models/model.py
+++ Sentimental_Analysis_for_Tweets_NLP/src/models/model.py
@@ -1,20 +1,15 @@
-import transformers
-from transformers import BertModel, BertTokenizer, AdamW, get_linear_schedule_with_warmup
+from transformers import BertModel, AdamW
+import torch.nn.functional as F
 import torch
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
+import pytorch_lightning as pl
 from matplotlib import rc
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix, classification_report
-from collections import defaultdict
 from textwrap import wrap
-
-from torch import nn, optim
-from torch.utils.data import Dataset, DataLoader
+from torch import nn
 import torch.nn.functional as F
 
-class DepressionClassifier(nn.Module):
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+class DepressionClassifier(pl.LightningModule):
 
   def __init__(self, n_classes, pre_trained_model_name):
     super(DepressionClassifier, self).__init__()
@@ -30,3 +25,25 @@ class DepressionClassifier(nn.Module):
     )
     output = self.drop(pooled_output)
     return self.out(output)
+
+  def training_step(self, train_batch, batch_idx):
+    input_ids = train_batch["input_ids"].to(device)
+    attention_mask = train_batch["attention_mask"].to(device)
+    labels = train_batch["depression"].to(device)
+    outputs = self.forward(input_ids, attention_mask)
+    loss = F.cross_entropy(outputs, labels)
+    self.log('train loss', loss)
+
+    return loss
+
+  def validation_step(self, val_batch, batch_idx):
+    input_ids = val_batch["input_ids"].to(device)
+    attention_mask = val_batch["attention_mask"].to(device)
+    labels = val_batch["depression"].to(device)
+    outputs = self.forward(input_ids, attention_mask)
+    loss = F.cross_entropy(outputs, labels)
+    self.log('train loss', loss)
+
+
+  def configure_optimizers(self):
+    return AdamW(self.parameters(), lr = 2e-5, correct_bias = False)
